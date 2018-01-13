bf558a17673f64805709e523a2eb4e352b34dca9
George Ciachir <george.ciachirarhire@endava.com>
2018-01-13 17:32:34 +0200

    Filter now routes based on the requested uri

--- zuul-dynamic-routing/src/main/java/george/projects/demos/zuul/dynamic/routing/filters/RoutingFilter.java
+++ zuul-dynamic-routing/src/main/java/george/projects/demos/zuul/dynamic/routing/filters/RoutingFilter.java
@@ -1,18 +1,23 @@
 package george.projects.demos.zuul.dynamic.routing.filters;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.FORWARD_TO_KEY;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.REQUEST_URI_KEY;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import javax.annotation.Resource;
 
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+import george.projects.demos.zuul.dynamic.routing.configuration.EnvironmentSettings;
+
 @Component
 public class RoutingFilter extends ZuulFilter {
+
+	@Resource
+	private EnvironmentSettings settings;
 
 	@Override
 	public String filterType() {
@@ -21,7 +26,7 @@ public class RoutingFilter extends ZuulFilter {
 
 	@Override
 	public int filterOrder() {
-		return 6;
+		return PRE_DECORATION_FILTER_ORDER + 1;
 	}
 
 	@Override
@@ -33,13 +38,10 @@ public class RoutingFilter extends ZuulFilter {
 	public Object run() {
 		RequestContext requestContext = RequestContext.getCurrentContext();
 
-		try {
-			requestContext.setRouteHost(new URL("https://spring.io/"));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
+		requestContext.setRouteHost(settings.getTargetedServiceUrl());
 		requestContext.remove(FORWARD_TO_KEY);
-		requestContext.put(REQUEST_URI_KEY, "/projects");
+		requestContext.put(REQUEST_URI_KEY, requestContext.getRequest().getRequestURI());
+
 		return null;
 	}
 }
