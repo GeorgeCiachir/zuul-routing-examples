3efab993d715bf895be8fbe93400579f825977d6
George Ciachir <george.ciachirarhire@endava.com>
2018-01-14 07:46:58 +0200

    Add controller and method with mapping

---
+++ first-targeted-service/src/main/java/george/projects/demos/first/targeted/service/controller/ApplicationController.java
@@ -0,0 +1,15 @@
+package george.projects.demos.first.targeted.service.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller(value = "/")
+public class ApplicationController {
+
+	@ResponseBody
+	@RequestMapping(value = "/firstTargetedService")
+	public String respond() {
+		return "Response from the First Service";
+	}
+}
