08a2ed33591258b6a37ee9d2d190df5af9694476
George Ciachir <george.ciachirarhire@endava.com>
2018-01-15 09:32:25 +0200

    Add eureka server module for discovery

---
+++ eureka-service/src/main/java/george/projects/demos/eureka/service/Application.java
@@ -0,0 +1,22 @@
+package george.projects.demos.eureka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServiceApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EurekaServiceApplication.class);
+	}
+
+}
